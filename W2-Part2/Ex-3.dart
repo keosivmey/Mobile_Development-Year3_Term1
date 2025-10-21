ef3165094045674b3bfe9844ecded8fdf056ef13
keosivmey <keosivmey77@gmail.com>
2025-10-22 00:11:32 +0700

    update on exercise3

--- W2-Part2/Ex-3.dart
+++ W2-Part2/Ex-3.dart
@@ -1,36 +1,49 @@
-//Exercise 3 - create a custom duration class
-class Duration {
-  final int _milliseconds; //default as milliseconds
-
-  Duration (this._milliseconds) {
-    if(this._milliseconds < 0) throw Exception('Duration must be greater than 0');
-  }
-
-  Duration.fromHours(int hours) : this(hours * 3600 * 1000);
-  Duration.fromMinutes(int minutes) : this(minutes * 60 * 1000);
-  Duration.fromSeconds(int seconds) : this(seconds * 1000); 
-
-  //getter for disferent duration
-  double get seconds => double.parse((this._milliseconds / 1000).toStringAsFixed(2));
-  double get minutes => double.parse((this._milliseconds / 60000).toStringAsFixed(2));
-  double get hours => double.parse((this._milliseconds / 3600000).toStringAsFixed(2));
-
-  //overload operator
-  //compare 2 duration 
-  bool operator >(Duration other) {
-    return this._milliseconds > other._milliseconds;
-  }
-
-  //add 2 duraions
-  Duration operator +(Duration other) {
-    return Duration(this._milliseconds + other._milliseconds);
-  }
-
-  //minus 2 duraions
-  Duration operator -(Duration other) {
-    if(this > other || this._milliseconds == other._milliseconds){
-        return Duration(this._milliseconds - other._milliseconds);
+class CustomDuration {
+  final int _milliseconds; // Stored as milliseconds
+  
+  //main constructor 
+  CustomDuration(this._milliseconds) {
+    if (_milliseconds < 0) {
+      throw Exception('Duration must be equal or greater than 0');
     }
-    throw Exception('The first duration must be greather than second duration.');
   }
+
+  //name constructor
+  CustomDuration.fromHours(int hours) : _milliseconds = hours * 3600 * 1000;
+  CustomDuration.fromMinutes(int minutes): _milliseconds = minutes * 60 * 1000;
+  CustomDuration.fromSeconds(int seconds): _milliseconds = seconds * 1000;
+
+  //getter 
+  double get seconds => _milliseconds / 1000;
+  double get minutes => _milliseconds / 60000;
+  double get hours => _milliseconds / 3600000;
+  
+  //operator
+  bool operator >(CustomDuration other) {
+    return _milliseconds > other._milliseconds;
+  }
+
+  CustomDuration operator +(CustomDuration other) {
+    return CustomDuration(_milliseconds + other._milliseconds);
+  }
+  
+  CustomDuration operator -(CustomDuration other) {
+    if (this > other || _milliseconds == other._milliseconds) {
+      return CustomDuration(_milliseconds - other._milliseconds);
+    }
+    throw Exception('The first duration must be greater than or equal to the second duration.');
+  }
+
+   @override
+  String toString() => 'CustomDuration($_milliseconds ms)';
+}
+
+  void main() {
+  var d1 = CustomDuration.fromMinutes(2);
+  var d2 = CustomDuration.fromSeconds(10);
+
+  print(d1 > d2); // true
+  print(d1 + d2); // 130000 ms
+  print(d1 - d2); // 110000 ms
+  print(d1.hours); // 0.033
 }
