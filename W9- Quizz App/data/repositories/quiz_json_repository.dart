f8c10d4cb4a6b630cc08e75669240c61d3d16292
keosivmey <keosivmey77@gmail.com>
2025-12-07 01:32:40 +0700

    Add files via upload

--- W9- Quizz App/data/repositories/quiz_json_repository.dart
+++ W9- Quizz App/data/repositories/quiz_json_repository.dart
@@ -1,1 +1,16 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import '../../model/quiz.dart';
 
+class QuizRepository {
+  final String assetPath;
+
+  QuizRepository({required this.assetPath});
+
+  //Load quiz from JSON
+  Future<Quizz> readQuiz() async {
+    final content = await rootBundle.loadString(assetPath);
+    final data = jsonDecode(content);
+    return Quizz.fromJson(data);
+  }
+}
