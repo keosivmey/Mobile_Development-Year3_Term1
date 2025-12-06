222b19432fd9f5f0bacbdd4ad925dab493018ccf
keosivmey <keosivmey77@gmail.com>
2025-12-07 01:29:36 +0700

    Add files via upload

---
+++ W9- Quizz App/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './ui/screen/quiz_app.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.blue.shade500,
+      ),
+      home: const QuizAppRoot(),
+    );
+  }
+}
